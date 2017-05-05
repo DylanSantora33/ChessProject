280a9a22bc0a4d21c27dcb157c82c2dc8312914e
DylanSantora <dmsantora1@gmail.com>
2017-05-05 09:06:59 -0700

    Update Queen.java

--- src/pieces/Queen.java
+++ src/pieces/Queen.java
@@ -1,1 +1,29 @@
+package pieces;
 
+public class Queen {
+	private int myColor;
+	private String myPieceType;
+	
+	public Queen(int color)
+	{
+		super();
+		setMyColor(color);
+		myPieceType = "bishop";
+	}
+
+	public int getMyColor() {
+		return myColor;
+	}
+
+	public void setMyColor(int color) {
+		myColor = color;
+	}
+
+	public String getMyPieceType() {
+		return myPieceType;
+	}
+
+	public void setMyPieceType(String pieceType) {
+		myPieceType = pieceType;
+	}
+}
