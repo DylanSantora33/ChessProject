203c27c28a65a3de7f22f8ca12f4e5bbb38152dd
DylanSantora <dmsantora1@gmail.com>
2017-05-05 09:01:12 -0700

    Update Bishop.java

--- src/pieces/Bishop.java
+++ src/pieces/Bishop.java
@@ -1,4 +1,27 @@
-
 public class Bishop {
+	private int myColor;
+	private String myPieceType;
+	
+	public Bishop(int color)
+	{
+		super();
+		setMyColor(color);
+		myPieceType = "bishop";
+	}
 
+	public int getMyColor() {
+		return myColor;
+	}
+
+	public void setMyColor(int color) {
+		myColor = color;
+	}
+
+	public String getMyPieceType() {
+		return myPieceType;
+	}
+
+	public void setMyPieceType(String pieceType) {
+		myPieceType = pieceType;
+	}
 }
