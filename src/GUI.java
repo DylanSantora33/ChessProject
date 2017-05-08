8b2f6c90a7b7b200ea5ce96663c44876f4eb0982
kevinsunchen <kevinsunchen0520@gmail.com>
2017-05-08 08:20:22 -0700

    changes made

--- src/GUI.java
+++ src/GUI.java
@@ -1,44 +1,44 @@
-
-
-
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
 import java.awt.Color;
 
 public class GUI extends JFrame {
-    
-	private JPanel panel1;
+
+    private JPanel panel1;
     private JLabel label1;
     private JButton[][] buttonArray;
     final static int NUM_ROWS = 8;
     final static int NUM_COLS = 8;
-    
-    /** Creates a new instance of GuiTest2 */
+
+    /**
+     * Creates a new instance of GuiTest2
+     */
     public GUI() {
 
         super("Button Grid Demo");
-        
+
 
         Container container = getContentPane();
-        container.setLayout( new GridLayout(NUM_ROWS, NUM_COLS) );
-        
+        container.setLayout(new GridLayout(NUM_ROWS, NUM_COLS));
+
         panel1 = new JPanel(new BorderLayout());
         buttonArray = new JButton[8][8];
         ButtonHandler buttonHandler = new ButtonHandler();
-        
-        for (int r = 0; r < NUM_ROWS; r++)
+
+        for (int r = 0; r < NUM_ROWS; r++) {
             for (int c = 0; c < NUM_COLS; c++) {
                 buttonArray[r][c] = new JButton();
 
-                if ((c+r) % 2 != 0)
-                buttonArray[r][c].setBackground(Color.black);
+                if ((c + r) % 2 != 0) {
+                    buttonArray[r][c].setBackground(Color.black);
+                }
                 container.add(buttonArray[r][c]);
 
                 buttonArray[r][c].addActionListener(buttonHandler);
 
             }
-        
+        }
 
 
         addWindowListener(new java.awt.event.WindowAdapter() {
@@ -46,27 +46,25 @@ public class GUI extends JFrame {
                 System.exit(0);
             }
         });
-        
 
-        
 
-        setSize( 900, 900);
+        setSize(900, 900);
         setVisible(true);
-        
+
     }
-    
+
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         GUI application = new GUI();
     }
-    
+
 
     private class ButtonHandler implements ActionListener {
-        public void actionPerformed (ActionEvent event) {
+        public void actionPerformed(ActionEvent event) {
 
-            for (int r = 0; r < NUM_ROWS; r++)
+            for (int r = 0; r < NUM_ROWS; r++) {
                 for (int c = 0; c < NUM_COLS; c++) {
                     if (event.getSource() == buttonArray[r][c]) {
                         System.out.println("Button at row " + r + ",col " + c + " pressed");
@@ -74,5 +72,6 @@ public class GUI extends JFrame {
                 }
             }
         }
-    
     }
+
+}
