8eee09ee78b16ca4f0b2eb6fa3505d4d8b1b2e27
kevinsunchen <kevinsunchen0520@gmail.com>
2017-05-10 09:49:43 -0700

    changes made

--- src/pieces/Bishop.java
+++ src/pieces/Bishop.java
@@ -2,17 +2,21 @@ package pieces;
 
 import java.util.ArrayList;
 
+import location.ChessBoard;
 import location.Location;
 
 public class Bishop extends ChessPiece {
     private int myColor;
     private String myPieceType;
     private ArrayList<Location> moveLocations;
+    private ChessBoard board;
+    private Location location;
 
     public Bishop(int color) {
         super();
         setMyColor(color);
         myPieceType = "bishop";
+        board = null;
     }
 
     public void getMoveLocations() {
@@ -21,19 +25,17 @@ public class Bishop extends ChessPiece {
 
     }
 
-    public int getMyColor() {
-        return myColor;
+    public void putSelfInBoard(ChessBoard b, Location l) {
+        if (board == null) {
+            if (l.isValid()) {
+                b.put(l, this);
+                board = b;
+                location = l;
+            }
+        }
     }
 
-    public void setMyColor(int color) {
-        myColor = color;
-    }
+    public ChessBoard getChessBoard() {
 
-    public String getMyPieceType() {
-        return myPieceType;
-    }
-
-    public void setMyPieceType(String pieceType) {
-        myPieceType = pieceType;
     }
 }
