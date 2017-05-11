e1644fd748b8d4c66c4e58e17b5db6548e525fd6
DylanSantora <dmsantora1@gmail.com>
2017-05-11 08:50:42 -0700

    Update Queen.java

--- src/pieces/Queen.java
+++ src/pieces/Queen.java
@@ -7,17 +7,11 @@ import java.util.ArrayList;
 public class Queen extends ChessPiece {
     private int myColor;
     private String myPieceType;
-    private ArrayList<Location> moveLocations;
 
     public Queen(int color) {
         super();
         setMyColor(color);
         myPieceType = "queen";
-    }
-
-    public void getMoveLocations() {
-        //TODO
-        moveLocations = new ArrayList<Location>();
     }
 
     public int getMyColor() {
