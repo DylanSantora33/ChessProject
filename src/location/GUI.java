82ef82647b91e6dec996f8fbfcc9b5520ba775fc
kevinsunchen <kevinsunchen0520@gmail.com>
2017-05-25 18:50:06 -0700

    changes made

--- src/location/GUI.java
+++ src/location/GUI.java
@@ -218,7 +218,7 @@ public class GUI extends JFrame {
         }
 
         public void actionPerformed(ActionEvent e) {
-            System.out.println("hiii");
+            System.out.println("New game started");
             GUI app = new GUI();
             myBoard = new ChessBoard();
             myBoard.populate();
